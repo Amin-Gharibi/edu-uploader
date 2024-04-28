8558b05001956b2c561d5b8049d0a196e5f67d2a
Amin Gharibi <gharibi8364@gmail.com>
2024-04-28 22:41:32 +0330

    create quick links page or the sidebar menu page full responsive and with create and delete function

--- public/scripts/funcs/sidebarMenu.js
+++ public/scripts/funcs/sidebarMenu.js
@@ -1,4 +1,5 @@
 import BASE_URL from "../util/BASE_URL.js";
+import { getToken } from "../util/utils.js";
 
 const getAllSidebarMenus = async () => {
 	const response = await fetch(`${BASE_URL}/api/sidebarMenu`)
@@ -6,6 +7,41 @@ const getAllSidebarMenus = async () => {
 	return await response.json()
 }
 
+const createSideBarMenu = async (title, href) => {
+	const sendingData = {
+		title,
+		href
+	}
+
+	const response = await fetch(`${BASE_URL}/api/sidebarMenu`, {
+		method: 'POST',
+		headers: {
+			'Authorization': `Bearer ${getToken()}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(sendingData)
+	})
+
+	const data = await response.json()
+
+	return {data, ok: response.ok}
+}
+
+const deleteSideBarMenu = async (id) => {
+	const response = await fetch(`${BASE_URL}/api/sidebarMenu/${id}`, {
+		method: 'DELETE',
+		headers: {
+			'Authorization': `Bearer ${getToken()}`
+		}
+	})
+
+	const data = await response.json()
+
+	return {data, ok: response.ok}
+}
+
 export {
-	getAllSidebarMenus
+	getAllSidebarMenus,
+	createSideBarMenu,
+	deleteSideBarMenu
 }
