e5ce195d9ded659c11f1ff804b2cce50dfa8c4d9
Amin Gharibi <gharibi8364@gmail.com>
2024-04-28 19:13:16 +0330

    create focused subject page in panel full responsive with create and delete functions

--- public/scripts/funcs/focusedSubjects.js
+++ public/scripts/funcs/focusedSubjects.js
@@ -1,4 +1,5 @@
 import BASE_URL from "../util/BASE_URL.js";
+import { getToken } from "../util/utils.js";
 
 const getOneFocusedSubject = async (enTitle) => {
 	const response = await fetch(`${BASE_URL}/api/focusedSubject/${enTitle}`)
@@ -12,7 +13,42 @@ const getAllFocusedSubjects = async () => {
     return await response.json()
 }
 
+const createFocusedSubject = async (title, enTitle) => {
+    const sendingData = {
+        title,
+        enTitle
+    }
+
+    const response = await fetch(`${BASE_URL}/api/focusedSubject`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${getToken()}`,
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(sendingData)
+    })
+
+    const data = await response.json()
+
+    return {data, ok: response.ok}
+}
+
+const deleteFocusedSubject = async (id) => {
+    const response = await fetch(`${BASE_URL}/api/focusedSubject/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${getToken()}`
+        }
+    })
+
+    const data = await response.json()
+
+    return {data, ok: response.ok}
+}
+
 export {
 	getOneFocusedSubject,
-    getAllFocusedSubjects
+    getAllFocusedSubjects,
+    createFocusedSubject,
+    deleteFocusedSubject
 }
