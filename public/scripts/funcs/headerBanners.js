5120a1d505492857123223adb303526e67967d72
Amin Gharibi <gharibi8364@gmail.com>
2024-04-28 22:12:02 +0330

    create header banners page in panel full responsive with create and delete functions

--- public/scripts/funcs/headerBanners.js
+++ public/scripts/funcs/headerBanners.js
@@ -1,4 +1,5 @@
 import BASE_URL from "../util/BASE_URL.js";
+import { getToken } from "../util/utils.js";
 
 const getHeaderBanners = async () => {
 	const response = await fetch(`${BASE_URL}/api/headerBanner`)
@@ -6,6 +7,40 @@ const getHeaderBanners = async () => {
 	return await response.json()
 }
 
+const createHeaderBanner = async (title, href, cover) => {
+	const sendingData = new FormData()
+	sendingData.append('title', title)
+	sendingData.append('href', href)
+	sendingData.append('cover', cover)
+
+	const response = await fetch(`${BASE_URL}/api/headerBanner`, {
+		method: 'POST',
+		headers: {
+			'Authorization': `Bearer ${getToken()}`
+		},
+		body: sendingData
+	})
+
+	const data = await response.json()
+
+	return {data, ok: response.ok}
+}
+
+const deleteHeaderBanner = async (id) => {
+	const response = await fetch(`${BASE_URL}/api/headerBanner/${id}`, {
+		method: 'DELETE',
+		headers: {
+			'Authorization': `Bearer ${getToken()}`
+		}
+	})
+
+	const data = await response.json()
+
+	return {data, ok: response.ok}
+}
+
 export {
-	getHeaderBanners
+	getHeaderBanners,
+	createHeaderBanner,
+	deleteHeaderBanner
 }
